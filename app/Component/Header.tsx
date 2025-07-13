0e9414fcd7fb4f0814e08d799fcf255fe5c8d5ac
ちょんまげ <qcomekei@gmail.com>
2025-07-13 17:58:58 +0900

    ヘッダーメニュー作成(仮)

---
+++ app/Component/Header.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+export default function Header() {
+    type Menu = {
+        label: string
+    };
+    const menu: Menu[] = [
+        { label: "ホーム" },
+        { label: "PickUp" },
+        { label: "こだわり" },
+        { label: "LineUp" },
+        { label: "楽しみ方" },
+        { label: "News" },
+        { label: "Shop" },
+        { label: "お問い合わせ" },
+    ];
+    return (
+        <header className="flex justify-between	 px-10 py-8">
+            <p>ロゴ</p>
+            <ul className="flex gap-10">
+                {menu.map((item, index) => (<li key={index}>{item.label}</li>))}
+            </ul>
+        </header>
+    )
+}
