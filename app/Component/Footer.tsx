c955d236c803852c1ce5a0a5cb2de152d4a0e400
ちょんまげ <qcomekei@gmail.com>
2025-07-21 11:31:37 +0900

    修正版

--- app/Component/Footer.tsx
+++ app/Component/Footer.tsx
@@ -1,15 +1,16 @@
 'use client';
 
+type Menu = {
+    label: string
+};
+const menu: Menu[] = [
+    { label: "ホーム" },
+    { label: "Access" },
+    { label: "Shop" },
+    { label: "お問い合わせ" },
+];
+
 export default function Footer() {
-    type Menu = {
-        label: string
-    };
-    const menu: Menu[] = [
-        { label: "ホーム" },
-        { label: "Access" },
-        { label: "Shop" },
-        { label: "お問い合わせ" },
-    ];
     return (
         <footer className="justify-between px-10 py-8">
             <ul className="flex justify-center gap-20 ">
--- app/Component/Header.tsx
+++ app/Component/Header.tsx
@@ -1,19 +1,19 @@
 'use client';
+type Menu = {
+    label: string
+};
+const menu: Menu[] = [
+    { label: "ホーム" },
+    { label: "PickUp" },
+    { label: "こだわり" },
+    { label: "LineUp" },
+    { label: "楽しみ方" },
+    { label: "News" },
+    { label: "Shop" },
+    { label: "お問い合わせ" },
+];
+export default function Header() {
 
-export default function header() {
-    type Menu = {
-        label: string
-    };
-    const menu: Menu[] = [
-        { label: "ホーム" },
-        { label: "PickUp" },
-        { label: "こだわり" },
-        { label: "LineUp" },
-        { label: "楽しみ方" },
-        { label: "News" },
-        { label: "Shop" },
-        { label: "お問い合わせ" },
-    ];
     return (
         <header className="flex justify-between	 px-10 py-8">
             <p>ロゴ</p>
