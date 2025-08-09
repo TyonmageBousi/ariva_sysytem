eb7d62b2a6a2f2c5775e472bc0d3134aa57c1117
Haruino <harukiinoue5270@gmail.com>
2025-08-10 01:10:11 +0900

    微修正

--- app/components/server/OurProductContainer.tsx
+++ app/components/server/OurProductContainer.tsx
@@ -15,7 +15,7 @@ export default async function OurProductContainer({ titleCss }: { titleCss: stri
   // モックからデータフェッチ
   const res = await fetch("http://localhost:3000/api/mock/our_thoughts")
   //ここ直す
-  const data: OurThought[] = res.json()
+  const data: OurThought[] = await res.json()
 
   // const filterdData = data.filter((x) => x.title !== "tomic")
 
