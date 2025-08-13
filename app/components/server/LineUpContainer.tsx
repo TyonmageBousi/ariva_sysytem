6e5d25b3c5b68ee929459816e34c61ed34f4c541
ちょんまげ <qcomekei@gmail.com>
2025-08-13 09:41:51 +0900

    メインページの画像・文章をモックサーバから呼び出し方式に変更しました

--- app/components/server/LineUpContainer.tsx
+++ app/components/server/LineUpContainer.tsx
@@ -1,13 +1,16 @@
 import { ProductsInfo } from "@/app/api/mock/line_up/route"
 import React from "react"
 import ProductLineUp from "../user/ProductLineUp"
-
-
+import { notFound } from "next/navigation";
 
 export default async function TopMainContainer({ titleCss }: { titleCss: string }) {
-    // モックからデータフェッチ
+
     const res = await fetch("http://localhost:3000/api/mock/line_up")
-    //ここ直す
+
+    if (res.status === 404) notFound();
+
+    if (!res.ok) throw new Error(`/api/mock/line_up fetch failed: ${res.status} ${res.statusText}`);
+
     const data: ProductsInfo[] = await res.json()
 
     return <ProductLineUp titleCss={titleCss} data={data} />
--- app/components/server/OurProductContainer.tsx
+++ app/components/server/OurProductContainer.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import OurProductThoughts from ".././user/OurProductThoughts"
+import { notFound } from "next/navigation";
+
 
 export type OurThought = {
   title: string;
@@ -8,9 +10,14 @@ export type OurThought = {
   src2: string;
 };
 export default async function OurProductContainer({ titleCss }: { titleCss: string }) {
-  // モックからデータフェッチ
+
   const res = await fetch("http://localhost:3000/api/mock/our_thoughts")
-  //ここ直す
+
+  if (res.status === 404) notFound();
+
+  if (!res.ok) throw new Error(`/api/mock/our_thoughts fetch failed: ${res.status} ${res.statusText}`);
+
+
   const data: OurThought[] = await res.json()
 
   return <OurProductThoughts titleCss={titleCss} data={data} />
